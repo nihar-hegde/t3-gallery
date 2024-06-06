ae46dccec66771e70b3b5dd372ed8427f1e0029e
Nihar Hegde <niharhegde163@gmail.com>
2024-06-06 21:15:06 +0530

    Delete files from uploadthing when deleting from database

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -6,6 +6,7 @@ import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import analyticsServerClient from "./analytics";
+import { utapi } from "./uploadthing";
 
 export const getMyImages = async () => {
   const user = auth();
@@ -33,6 +34,19 @@ export const getImage = async (id: number) => {
 export async function deleteImage(id: number) {
   const user = auth();
   if (!user.userId) throw new Error("Unauthorized");
+
+  const image = await db.query.images.findFirst({
+    where: (model, { eq }) => eq(model.id, id),
+  });
+  if (!image) {
+    throw new Error("Image not found");
+  }
+  const extractedPart = image.url.split("/f/")[1];
+  if (!extractedPart) throw new Error("Image Url Not Found");
+
+  const deleteFileInUploadthingServer = await utapi.deleteFiles(extractedPart);
+  console.log(deleteFileInUploadthingServer);
+
   await db
     .delete(images)
     .where(and(eq(images.id, id), eq(images.userId, user.userId)));
